e96c849873bc8450af896afe3c9229c38cb30c9c
duduzinmuller <eduardomuller989@gmail.com>
2025-07-17 17:48:27 -0400

    feat: add  CreateUserUseCase

---
+++ src/use-case/user.js
@@ -0,0 +1,19 @@
+import { v4 as uuidV4 } from "uuid";
+import { CreateUserRepository } from "../repositorio/user";
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    const userId = uuidV4();
+
+    const user = {
+      id: userId,
+      ...createUserParams,
+    };
+
+    const createUserRepository = new CreateUserRepository();
+
+    const result = await createUserRepository.execute(user);
+
+    return result;
+  }
+}
